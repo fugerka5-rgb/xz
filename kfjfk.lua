68afd7e12121478c5ce9ae8e2406fecc45938f87
Monstar767 <fugerka5@gmail.com>
2025-12-16 00:37:06 +0200

    Update kfjfk.lua

--- kfjfk.lua
+++ kfjfk.lua
@@ -1,4 +1,3 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "Rayfield Example Window",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
